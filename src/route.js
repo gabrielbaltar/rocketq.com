9c710ed5145244aca757461d1cca1d2dd8a64b3f
Gabriel Baltar <gabriel.baltar21@hotmail.com>
2022-04-19 03:34:46 -0300

    26

--- src/route.js
+++ src/route.js
@@ -10,14 +10,15 @@ const QuestionController = require('./controllers/QuestionController');
 
 const route = express.Router();
 
-route.get('/', (req, res) => res.render("index"));
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}));
+route.get('/createPass', (req, res) => res.render('index', {page: 'createPass'}));
 route.get('/room', (req, res) => res.render('room'));
-route.get('/createPass', (req, res) => res.render('createPass'));
 
 
 
 // Formato que o formulário de dentro da modal tem que passar a informação;
 route.post('/room/:room/:question/:action', QuestionController.index);
+route.post('/room/create´room', RoomController.create);
 
 // Exportando as routes
 
